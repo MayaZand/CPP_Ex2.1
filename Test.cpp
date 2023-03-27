f3733cefb208fd10dee48434297d60758ecbc1f7
MayaZand <118796716+MayaZand@users.noreply.github.com>
2023-03-27 13:53:22 +0300

    #Final commit

--- Test.cpp
+++ Test.cpp
@@ -8,39 +8,39 @@
 #include "sources/game.hpp"
 #include "sources/card.hpp"
 
-using namespace std;
-
-
 using namespace ariel;
 using namespace std;
 
-
-
 TEST_CASE ("befor the game starts")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2);
+    Player player1 ("Alice");
+    Player player2 ("Bob");
     
-    CHECK (p1.stacksize() == 26); //At the beginning of the game, each player should have 26 cards
-    CHECK (p2.stacksize() == 26); //At the beginning of the game, each player should have 26 cards
-    CHECK (p1.cardesTaken() == 0); //At the beginning of the game, none of the players won a card
-    CHECK (p2.cardesTaken() == 0); //At the beginning of the game, none of the players won a card
-    CHECK (p1.stacksize() + p2.stacksize() == 52); //The sum of cards of both players should be 52 card
+    CHECK (player1.stacksize() == 0); //Before the players start play they don't have any cards
+    CHECK (player2.stacksize() == 0); //Before the players start play they don't have any cards
+    CHECK (player1.cardesTaken() == 0); //At the beginning of the game, none of the players won a card
+    CHECK (player2.cardesTaken() == 0); //At the beginning of the game, none of the players won a card
+    Game game (player1, player2); //Create new game with Alice and Bob
+    
+    CHECK (player1.stacksize() == 26); //Before the game starts, each player should have 26 cards
+    CHECK (player2.stacksize() == 26); //Before the game starts, each player should have 26 cards
+    CHECK (player1.cardesTaken() == 0); //At the beginning of the game, none of the players won a card
+    CHECK (player2.cardesTaken() == 0); //At the beginning of the game, none of the players won a card
+    CHECK (player1.stacksize() + player2.stacksize() == 52); //The sum of cards of both players should be 52 card
 }
 
 TEST_CASE("during the game")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2);
+    Player player1 ("Alice");
+    Player player2 ("Bob");
+    Game game (player1, player2);
     game.playTurn();
     bool gameStarts=false;
 
-    CHECK (p1.stacksize() < 26); //After the game starts, both players have less than 26 card
-    CHECK (p2.stacksize() < 26); //After the game starts, both players have less than 26 card
-    CHECK (p1.stacksize() +p1.cardesTaken() + p2.stacksize() + p2.cardesTaken() == 52); //Total number of cards should be 52
-    if (p1.cardesTaken() > 0 || p2.cardesTaken()>0)
+    CHECK (player1.stacksize() < 26); //After the game starts, both players have less than 26 card
+    CHECK (player2.stacksize() < 26); //After the game starts, both players have less than 26 card
+    CHECK (player1.stacksize() +player1.cardesTaken() + player2.stacksize() + player2.cardesTaken() == 52); //Total number of cards should be 52
+    if (player1.cardesTaken() > 0 || player2.cardesTaken()>0)
     {
         gameStarts = true;
     }
@@ -49,17 +49,17 @@ TEST_CASE("during the game")
 
 TEST_CASE ("end of the game")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2); //create new game
+    Player player1 ("Alice");
+    Player player2 ("Bob");
+    Game game (player1, player2); //create new game
     game.playAll(); //play the game until the end
     bool winner=false;
     
     
-    CHECK (p1.stacksize() == 0); //At the end of the game, each player should have 0 cards
-    CHECK (p2.stacksize() == 0); //At the end of the game, each player should have 0 cards
-    CHECK (p1.cardesTaken() + p2.cardesTaken() == 52); //Total sum of cards should be 52
-    if (p1.cardesTaken() > 0 || p2.cardesTaken()>0)
+    CHECK (player1.stacksize() == 0); //At the end of the game, each player should have 0 cards
+    CHECK (player2.stacksize() == 0); //At the end of the game, each player should have 0 cards
+    CHECK (player1.cardesTaken() + player2.cardesTaken() == 52); //Total sum of cards should be 52
+    if (player1.cardesTaken() > 0 || player2.cardesTaken()>0)
     {
         winner = true;
     }
@@ -68,67 +68,67 @@ TEST_CASE ("end of the game")
 
 TEST_CASE ("after 5 turns")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2); //create new game
+    Player player1 ("Alice");
+    Player player2 ("Bob");
+    Game game (player1, player2); //create new game
     bool winner;
     for (int i =1; i<=5; i++)
     {
         game.playAll();
     }
 
-    CHECK (p1.stacksize() <=21); //After 5 turns number of cards must be equal or less than 21 cards 
-    CHECK (p2.stacksize() <=21); //After 5 turns number of cards must be equal or less than 21 cards 
-    if (p1.cardesTaken() > 0 || p2.cardesTaken()>0)
+    CHECK (player1.stacksize() <=21); //After 5 turns number of cards must be equal or less than 21 cards 
+    CHECK (player2.stacksize() <=21); //After 5 turns number of cards must be equal or less than 21 cards 
+    if (player1.cardesTaken() > 0 || player2.cardesTaken()>0)
     {
         winner = true;
     }
-    CHECK (winner==true);
+    CHECK (winner);
 }
 
 TEST_CASE ("after 10 turns")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2); //create new game
+    Player player1 ("Alice");
+    Player player2 ("Bob");
+    Game game (player1, player2); //create new game
     bool winner;
     for (int i =1; i<=10; i++)
     {
         game.playTurn();
     }
 
-    CHECK (26 - p1.stacksize() >=16); //After 5 turns number of cards must be equal or less than 21 cards 
-    CHECK (26 - p2.stacksize() >=16); //After 5 turns number of cards must be equal or less than 21 cards 
-    if (p1.cardesTaken() > 0 || p2.cardesTaken()>0)
+    CHECK (26 - player1.stacksize() >=16); //After 5 turns number of cards must be equal or less than 21 cards 
+    CHECK (26 - player2.stacksize() >=16); //After 5 turns number of cards must be equal or less than 21 cards 
+    if (player1.cardesTaken() > 0 || player2.cardesTaken()>0)
     {
         winner = true;
     }
-    CHECK (winner==true);
+    CHECK (winner);
 }
 
 TEST_CASE ("each game is maximum 26 rounds")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2); //create new game
+    Player player1 ("Alice");
+    Player player2 ("Bob");
+    Game game (player1, player2); //create new game
     bool max26turns = true;
 
-    game.playAll();
+    game.playAll(); 
     CHECK_THROWS(game.playTurn()); //After max 26 rounds, it is not possible to play another turn
 }
 
 TEST_CASE ("An attempt to put the same player into one game")
 {
-    Player p2 ("Bob");
-    Game game (p2, p2); //Create new game with the same player
+    Player player2 ("Bob");
+    Game game (player2, player2); //Create new game with the same player
     CHECK_THROWS(game.playTurn()); //Should throw exception
 }
 
 TEST_CASE ("befor calling game.playTurn method")
 {
-    Player p1 ("Alice");
-    Player p2 ("Bob");
-    Game game (p1, p2); //create new game
+    Player player1 ("Alice");
+    Player player2 ("Bob");
+    Game game (player1, player2); //create new game
     CHECK_THROWS(game.printLastTurn());
     CHECK_THROWS(game.printWiner());
 }
